93270fbbcaffea1a16ea436d93179ae30e96c867
tusha-p <45887071+tusha-p@users.noreply.github.com>
2025-08-18 17:10:51 -0700

    Update CLI.java

--- src/main/java/com/yourgithubid/validator/CLI.java
+++ src/main/java/com/yourgithubid/validator/CLI.java
@@ -1,5 +1,7 @@
 package com.yourgithubid.validator;
 
+import java.io.File;
+
 public class CLI {
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -7,9 +9,11 @@ public class CLI {
             return;
         }
         try {
-            PDFValidator.validate(new java.io.File(args[0]));
+            // Convert String path to File object
+            PDFValidator.validate(new File(args[0]));
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
